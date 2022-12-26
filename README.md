8140f03e3b9754aaa8c57709e53f054b843afddb
Alexander Korobov <dktwho@mail.ru>
2022-12-26 17:09:21 +0300

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Содержание
+
+1. Вывод значения радиокнопки в параграф
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState(1)
+
+  const [value2, setValue2] = useState(0)
+
+  function changeHandler(e) {
+    setValue(e.target.value)
+  }
+
+  function changeHandler2(e) {
+    setValue2(e.target.value)
+  }
+
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input  
+            type="radio" 
+            name="radio"
+            value='1'
+            checked={value === '1' ? true : false}
+            onChange={changeHandler}
+          />1 radio
+
+          <input 
+            type="radio" 
+            name="radio"
+            value='2'
+            checked={value === '2' ? true : false}
+            onChange={changeHandler}
+          /> 2 radio
+
+          <input 
+            type="radio" 
+            name="radio"
+            value='3'
+            checked={value === '3' ? true : false}
+            onChange={changeHandler}
+          /> 3 radio
+
+          <p> Значение : {value}</p>
+
+          <br />
+          <br />
+          <hr />
+
+          <input  
+            type="radio" 
+            name="radio"
+            value='1'
+            checked={value2 === '1' ? true : false}
+            onChange={changeHandler2}
+          /> Java
+
+          <input 
+            type="radio" 
+            name="radio"
+            value='2'
+            checked={value2 === '2' ? true : false}
+            onChange={changeHandler2}
+          /> Go
+
+          <input 
+            type="radio" 
+            name="radio"
+            value='3'
+            checked={value2 === '3' ? true : false}
+            onChange={changeHandler2}
+          /> Ruby
+
+      <p>Favorite language: 
+        {value2 === '1' && 'Java'}
+        {value2 === '2' && 'Go'}
+        {value2 === '3' && 'Ruby'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
